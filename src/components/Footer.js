0720d73d79ed82475cb7dea6803e2b45593f78c2
kamil079 <kamil.szczepaniec@wp.pl>
2022-09-21 21:11:04 +0200

    done but missing funcionality..

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <section className="footer">
+            <div>
+                <span>
+                    Terms of service &#8901; Privacy policy &#8901; Copyright
+                </span>
+            </div>
+            <div>
+                <span>
+                    nerds.family Version 1.2 &#8901; Last update 10/09/2022
+                </span>
+            </div>
+        </section>
+    );
+};
+export default Footer;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,16 @@
 import Tasks from "./Tasks";
 import Context from "./Context";
+import Footer from "./Footer";
 
 const Main = () => {
     return (
-        <div className="main-container">
-            <Tasks/>
-            <Context/>
-        </div>
+        <>
+            <div className="main-container">
+                <Tasks/>
+                <Context/>
+            </div>
+            <Footer/>
+        </>
     );
 };
 
